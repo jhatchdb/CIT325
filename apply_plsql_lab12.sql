777e85107c4639fb4b99b18aa60672ec5fa2cd18
jhatchdb <58750829+jhatchdb@users.noreply.github.com>
2019-12-10 14:35:49 -0700

    Update apply_plsql_lab12.sql

--- apply_plsql_lab12.sql
+++ apply_plsql_lab12.sql
@@ -1,6 +1,5 @@
 /*
 ||  Name:          apply_plsql_lab12.sql
-||  Date:          11 Nov 2016
 ||  Purpose:       Complete 325 Chapter 13 lab.
 ||  Dependencies:  Run the Oracle Database 12c PL/SQL Programming setup programs.
 */
